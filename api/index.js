92cdd109f22fa3b18751be4ac02404e3c5e2bdac
abdul <idbsch2012@gmail.com>
2024-03-28 00:09:10 -0400

    connected to mongodb using the env variable

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+app.use(cors);
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Coonected to mongodb")
+}).catch((error)=>{
+    console.log(error)
+})
+
+app.listen(3000, ()=>{
+    console.log("App is listening on Port 3000")
+})
